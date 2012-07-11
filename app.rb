09cf7cd3d35d8aa827cc3a4db954ec07ca187755
Jarrod Peace <peace.jarrod@gmail.com>
2012-07-11 04:46:53 -0500

    Trying out a few things with includes

--- app.rb
+++ app.rb
@@ -3,6 +3,42 @@ require 'json'
 
 class App < Sinatra::Base
 
+  helpers do
+    def js_includes
+      @scripts = {
+        :js => [
+          'jquery', 'amplify', 'underscore'
+        ],
+        :coffee => [
+          :titan, {:core => [
+            :amplify, :bootstrap, :presenter, :view
+          ]}
+        ]
+      }
+      script_tags = ''
+      each_script @scripts[:js] do |path|
+        script_tags += %{<script type="text/javascript" src="js/#{path}.js"></script>}
+      end
+      each_script @scripts[:coffee] do |path|
+        script_tags += %{<script type="text/javascript" src="js/#{path}.js"></script>}
+      end
+
+      return script_tags
+    end
+
+    def each_script(scripts, path = '', &block)
+      scripts.each do |s|
+        if s.is_a? Hash
+          s.each do |folder, files|
+            each_script(files, "#{path}#{folder}/", &block)
+          end
+        else
+          block.call("#{path}#{s}")
+        end
+      end
+    end
+  end
+
   get '/js/?*?/:name.js' do
     content_type 'text/javascript', :charset => 'utf-8'
     coffee(:"coffee/#{params[:splat].join('/')}/#{params[:name]}")
