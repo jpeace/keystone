6e3d6dba0a03c26b753484df44581a17bdb73918
Jarrod Peace <peace.jarrod@gmail.com>
2012-11-12 10:46:41 -0600

    Working with paths on asset pipeline

--- infrastructure/assets/asset_loader.rb
+++ infrastructure/assets/asset_loader.rb
@@ -1,6 +1,8 @@
 module MusicOne
   module Assets
     class AssetLoader
+      attr_reader :assets
+
       class << self
         def type_from_filename(filename)
           extensions = {
@@ -16,6 +18,37 @@ module MusicOne
             :unknown
           else
             found[0]
+          end
+        end
+
+        def name_from_filename(filename)
+          filename.split('.').first
+        end
+      end
+
+      def initialize
+        @assets = []
+      end
+
+      def scan!(path)
+        scan_path(path)
+      end
+
+      private
+
+      def scan_path(path)
+        Dir.foreach(path) do |file|
+          next if ['.', '..'].include?(file)
+
+          full_path = "#{path}/#{file}"
+          if File.directory?(full_path)
+            scan_path(full_path)
+          else
+            last_slash = full_path.rindex('/')
+            path = full_path[0,last_slash]
+            filename = full_path[last_slash, -1]
+            puts path
+            puts filename
           end
         end
       end
