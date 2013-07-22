aadcd1f2b3f97f081f94348dd9419834b18c30fe
Tim Shelburne <shelburt02@gmail.com>
2013-07-22 08:38:33 -0500

    broke server out into manageable functions to make updating easier

--- lib/keystone/server.rb
+++ lib/keystone/server.rb
@@ -10,83 +10,37 @@ module Keystone
     @@mime_types[Keystone::Types::Css] = 'text/css'
 
     class << self
-      def pipeline
-        @@pipeline
+      attr_accessor :pipeline
+    end
+
+    get '*' do
+      return [404, 'Not Found'] if @@pipeline.nil?
+      
+      @@pipeline.compilers.each do |compiler|
+        safe_compile! compiler
+        
+        asset = find_asset_in_compiler compiler
+
+        unless asset.nil?
+          content_type @@mime_types[asset.type], :charset => 'utf-8'
+          return [200, asset.content]
+        end
       end
-      def pipeline=(pipeline)
-        @@pipeline = pipeline
-      end
+
+      [404, 'Not Found']
+    end
+
+    module Helpers
       
       # Forces a reload from disk if compilation fails
       def safe_compile!(compiler)
         begin
           compiler.compile!
         rescue
-          rebuild_hashes!(compiler)
+          recompile_assets!(compiler)
         end
-      end    
+      end 
 
-      # Reloads files from disk and recalculates MD5 hashes
-      def rebuild_hashes!(compiler)
-        compiler.reset!
-        compiler.compile!
-        compiler.assets.each do |a|
-          path_and_name = a.path == '' ? a.name : "#{a.path}/#{a.name}"
-          @@asset_hashes[path_and_name] = a.current_hash
-        end
-      end    
-    end
-
-    get '*' do
-      return [404, 'Not Found'] if @@pipeline.nil?
-      
-      requested_path = params[:splat].first[1..-1]
-      requested_filename = requested_path.split('/').last
-      requested_type = AssetLoader.type_from_filename(requested_filename)
-      asset = nil
-
-      @@pipeline.compilers.each do |c|
-        Keystone::Server.safe_compile!(c)
-        
-        asset = c.asset(requested_path)
-        unless asset.nil?
-          asset = nil unless asset.type == requested_type
-        end
-
-        if !asset.nil? && (asset.current_hash != @@asset_hashes[requested_path])
-          Keystone::Server.rebuild_hashes!(c)
-          asset = c.asset(requested_path)
-          unless asset.nil?
-            asset = nil unless asset.type == requested_type
-          end
-        end
-
-        if asset.nil?
-          parsed = /^((?:\w+\/)*)(\w+)(\.(\w+))?$/.match(requested_path)
-          path = parsed[1].gsub(/\/$/,'')
-          name = parsed[2]
-          extension = parsed[4]
-
-          results = c.external_assets.select{ |a| a.path == path && a.name == name }
-          if extension.nil?
-            asset = results.first
-          else
-            asset = results.find{ |a| a.type == Keystone::AssetLoader.type_from_extension(extension) }
-          end
-        end
-      
-        break unless asset.nil?
-      end
-
-      if asset.nil?
-        [404, 'Not Found']
-      else
-        content_type @@mime_types[asset.type], :charset => 'utf-8'
-        [200, asset.content]
-      end
-    end
-
-    module Helpers
       def self.included(mod)
         mod.helpers do
           def load_asset(asset_name, options={})
@@ -96,7 +50,7 @@ module Keystone
             raise "No compiler found for #{asset_name}" if compiler.nil?
 
             if ENV['RACK_ENV'] == 'development'
-              Keystone::Server.safe_compile!(compiler)
+              safe_compile!(compiler)
               tags = []
               [compiler.assets, compiler.external_assets].each do |assets|
                 assets.each do |a|
@@ -130,5 +84,69 @@ module Keystone
         end
       end
     end
+
+
+    private
+
+    def requested_path
+      @requested_path ||= params[:splat].first[1..-1]
+    end
+
+    def requested_filename
+      @requested_filename ||= requested_path.split('/').last
+    end
+
+    def requested_type
+      @requested_type ||= AssetLoader.type_from_filename(requested_filename)
+    end
+      
+    # Forces a reload from disk if compilation fails
+    def safe_compile!(compiler)
+      begin
+        compiler.compile!
+      rescue
+        recompile_assets!(compiler)
+      end
+    end    
+
+    # Reloads files from disk and recalculates MD5 hashes
+    def recompile_assets!(compiler)
+      compiler.reset!
+      compiler.compile!
+      compiler.assets.each do |a|
+        path_and_name = a.path == '' ? a.name : "#{a.path}/#{a.name}"
+        @@asset_hashes[path_and_name] = a.current_hash
+      end
+    end 
+
+    def find_asset_in_compiler compiler
+      asset = compiler.asset(requested_path)
+
+      if asset.nil?
+        asset = find_asset_in_externals
+      elsif asset.type == requested_type
+        if need_to_rebuild_asset? asset
+          recompile_assets! compiler
+          asset = compiler.asset(requested_path)
+        end
+      end
+
+      asset
+    end
+
+    def find_asset_in_externals
+      parsed = /^((?:\w+\/)*)(\w+)(\.(\w+))?$/.match(requested_path)
+      path = parsed[1].gsub(/\/$/,'')
+      name = parsed[2]
+      extension = parsed[4]
+
+      results = compiler.external_assets.select{ |asset| asset.path == path && asset.name == name }
+      extension.nil? ? results.first : results.find{ |asset| asset.type == Keystone::AssetLoader.type_from_extension(extension) }
+    end
+
+    def need_to_rebuild_asset? asset
+      asset.current_hash != @@asset_hashes[requested_path]
+    end
+
   end
 end
